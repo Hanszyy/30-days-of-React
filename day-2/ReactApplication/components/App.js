6b3a6fc5c1a97e0fec5b2f4b923d9ebcf7f45ad4
hanszyy <hanswong117@gmail.com>
2024-02-14 19:30:45 +0800

    JSX split commit

---
+++ day-2/ReactApplication/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello, you son of a beach!</h1>
+            <p>This is my first React app.</p>
+        </div>
+    );
+}
+
+export default App;
---
+++ day-2/ReactApplication/index.js
@@ -0,0 +1,8 @@
+/* This code is to contain Javascript code to initialize the React Application */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.js';
+
+ReactDOM.render(<bundle/>, document.getElementById('root'));
+
