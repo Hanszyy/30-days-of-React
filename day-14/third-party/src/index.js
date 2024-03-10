1b5f5470eabf71944c6f9cf7cd595a79a87d2bce
hanszyy <hanswong117@gmail.com>
2024-03-10 16:18:23 +0800

    simple 3rd party chart

---
+++ day-14/third-party/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BarChart from './chart';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BarChart />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
